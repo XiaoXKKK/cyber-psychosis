2d35079d981651cc8660921673f112ac5a861055
XiaoXKKK <37475561+XiaoXKKK@users.noreply.github.com>
2023-10-13 15:35:47 +0800

    AI 对话迭代
    用json做两边的data传输载体

--- src/Cyber Project 2D/Assets/Python/gpt_test.py
+++ src/Cyber Project 2D/Assets/Python/gpt_test.py
@@ -1,29 +1,23 @@
-'''
-curl https://api.openai-proxy.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
-  -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "Hello!"}]
-  }'
-
-'''
-
-
 import socket
 import sys
 import os
+import json
 
-from gpt_moreres import ChatGPT
+from gpt import Agent
 
 HOST = '127.0.0.1'
 PORT = 31415
 
-# os.environ['OPENAI_API_BASE']="https://api.chatanywhere.com.cn/v1/"
-
-chat = ChatGPT("test")
-
 log_file = open("log.txt", "w", encoding="utf-8")
+
+with open("Evelyn.json", "r", encoding="utf-8") as json_file:
+        loaded_data = json.load(json_file)
+Evelyn_Agent = Agent.from_json(loaded_data)
+
+def save_to_json(data, filename):
+    with open(filename, 'w',encoding="utf-8") as json_file:
+        json.dump(data, json_file, indent=4,ensure_ascii=False)  # 使用缩进使 JSON 文件更易读
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
@@ -34,10 +28,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         sys.stdout.flush()
         data, addr = s.recvfrom(1024)
         if(data.decode()):
-            print(data.decode(), file=log_file, flush=True)
-            chat.messages.append({"role": "user", "content": data.decode()})
-            answer = chat.ask_gpt()
-            # answer = "你好！测试中文"
-            print(answer, file=log_file, flush=True)
-            s.sendto(answer.encode(), (HOST, 5768))
-            print(f"[ChatGPT]{answer}", flush=True)
+            json_data = json.loads(data.decode())
+            # 根据json_data["npc"]找到对应的agent
+            answer = Evelyn_Agent.ask_gpt(json_data["content"])
+            s.sendto(json.dumps(answer, ensure_ascii=False).encode(), (HOST, 5768))
+            # save_to_json(Evelyn_Agent.to_json(),"Evelyn.json")
